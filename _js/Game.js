a58813505874d5e826e1b140dae660caa4b6e55f
Michelle Anderson <michelle@picot.ca>
2015-07-18 21:46:51 +0100

    capturing mouse move events

--- _js/Game.js
+++ _js/Game.js
@@ -60,9 +60,67 @@ Game.prototype.stepThroughShipPlacement = function() {
 }
 
 Game.prototype.makeShipClickable = function(shipId) {
+
   var shipEl = document.getElementById(shipId);
-  shipEl.onmousedown = function() {
-    console.log("you clicked on ", shipId);
+
+  shipEl.addEventListener('mousedown', drag);
+}
+
+
+function drag(e) {
+
+  console.log(this);
+  this.style.top = event.offsetTop;
+  this.style.left = event.offsetLeft;
+}
+
+document.onmousemove = mouseMove;
+document.onmouseup   = mouseUp;
+
+var dragObject  = null;
+var mouseOffset = null;
+
+function getMouseOffset(target, ev){
+  ev = ev || window.event;
+  var docPos    = getPosition(target);
+  var mousePos  = mouseCoords(ev);
+  return {x:mousePos.x - docPos.x, y:mousePos.y - docPos.y};
+}
+
+function getPosition(e){
+  var left = 0;
+  var top  = 0;
+  while (e.offsetParent){
+    left += e.offsetLeft;
+    top  += e.offsetTop;
+    e     = e.offsetParent;
+  }
+  left += e.offsetLeft;
+  top  += e.offsetTop;
+  return {x:left, y:top};
+}
+
+function mouseMove(ev){
+  ev           = ev || window.event;
+  var mousePos = mouseCoords(ev);
+  if(dragObject){
+    dragObject.style.position = 'absolute';
+    dragObject.style.top      = mousePos.y - mouseOffset.y;
+    dragObject.style.left     = mousePos.x - mouseOffset.x;
+    return false;
+  }
+}
+
+function mouseUp(){
+  dragObject = null;
+}
+
+function makeDraggable(item){
+  if(!item) return;
+  item.onmousedown = function(ev){
+    dragObject  = this;
+    mouseOffset = getMouseOffset(this, ev);
+    return false;
   }
 }
 
